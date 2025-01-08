b3516123e45a1cd4d18210c7425db3a4721f3887
hazekiller <prakashtimilsina76@gmail.com>
2025-01-08 15:37:41 +0545

    models updated in kura app

--- kuras/kura/models.py
+++ kuras/kura/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db import model
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Kura(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=240) 
+    photo = models.ImageField(upload_to='kura_photos', null=True, blank=True)   
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.text[:20]}"
